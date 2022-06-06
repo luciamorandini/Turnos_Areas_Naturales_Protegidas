fa1744eac7681c094e21fe11432372666e96b727
luciamorandini <62898085+luciamorandini@users.noreply.github.com>
2022-06-05 21:39:55 -0300

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,45 @@
+document.querySelectorAll(".table-input").forEach(el => {
+    el.addEventListener("click", e => {
+    console.log(el);
+      if((e.target).classList.contains("table-input")){
+        console.log("si");
+      }  
+      e.classList.add("selected");
+      const id = e.target.getAttribute("id");
+      /*console.log(e.target);*/
+    });
+});
+
+function select(){
+    var tabla_disponibles = document.getElementById('tabla-disponibles');
+}
+
+var tabla = document.querySelectorAll(".table-input")
+//console.log(tabla)
+
+function validar(){
+
+}
+
+
+function consultar(){
+    var tabla_disponibles = document.getElementById('tabla-disponibles');
+    tabla_disponibles.style.display = 'block';
+}
+
+
+
+
+
+/*var abrirVentana = document.querySelector("#abrir-ventana");
+var cerrarVentana = document.querySelector("#cerrar-ventana");
+var ventanaTerminar = document.querySelector("#ventana-terminar");
+var message = document.querySelector('.contenido-ventana');
+
+abrirVentana.addEventListener("click", function(){
+    ventanaTerminar.showModal();
+})
+
+cerrarVentana.addEventListener("click", function(){
+    ventanaTerminar.close();
+})*/
